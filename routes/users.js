8dd1892ca504ea9f3b700991caf5bbd2d1adba0e
Vishal Jain <jainvishal520@gmail.com>
2019-12-08 20:34:31 +0530

    Add validations in User route

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,29 @@
 const express = require("express")
 const router = express.Router()
+const { check, validationResult } = require("express-validator/check")
 const User = require("../models/User")
+
 // @route   POST    api/users
 // @desc    Register a new user
 // @access  Public
-
-router.post("/", (req, res) => {
-  res.send(req.body)
-})
+router.post(
+  "/",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Enter password with 6 or more characters").isLength({
+      min: 6
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    res.send("passed")
+  }
+)
 
 module.exports = router
