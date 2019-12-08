e7688916e0790f58495831c9804fff12c48833c1
vishal1jain <vishal1.jain@paytm.com>
2019-12-08 17:54:25 +0530

    Initial setup

---
+++ server.js
@@ -0,0 +1,12 @@
+// We don't use import syntax here because we will need babel then which we use in FE side 
+// i.e react, so using commonjs and require statements
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.json({ msg: 'Welcome to Contact Keeper API' }));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`)
+})
