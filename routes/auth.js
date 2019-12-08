ec751f033bc0ebf47404b752e2381295c705996d
Vishal Jain <jainvishal520@gmail.com>
2019-12-08 18:24:16 +0530

    Defining initial routes

---
+++ routes/auth.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/auth
+// @desc    Get logged in user
+// @access  Public
+
+router.get('/', (req, res) => {
+    res.send('Get logged in user')
+});
+
+// @route   POST api/auth
+// @desc    Log in user
+// @access  Private
+
+router.post('/', (req, res) => {
+    res.send('Log in user')
+})
+
+module.exports = router;
