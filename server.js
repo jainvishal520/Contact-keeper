40ea7be89a08053fca78057c57c1751061b68414
Vishal Jain <jainvishal520@gmail.com>
2019-12-08 18:46:51 +0530

    Database connection

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-// We don't use import syntax here because we will need babel then which we use in FE side 
+// We don't use import syntax here because we will need babel then which we use in FE side
 // i.e react, so using commonjs and require statements
-const express = require('express');
+const express = require("express")
+const connectDB = require("./config/db")
+const app = express()
 
-const app = express();
+//Connect database
+connectDB()
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to Contact Keeper API' }));
+app.get("/", (req, res) => res.json({ msg: "Welcome to Contact Keeper API" }))
 
 //Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use("/api/users", require("./routes/users"))
+app.use("/api/auth", require("./routes/auth"))
+app.use("/api/contacts", require("./routes/contacts"))
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-    console.log(`Server started at port ${PORT}`)
+  console.log(`Server started at port ${PORT}`)
 })
