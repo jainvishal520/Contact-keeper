0399d480225609695b144ecf9d943ccf48d42f1b
Vishal Jain <jainvishal520@gmail.com>
2019-12-14 20:36:52 +0530

    React setup and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import About from './components/pages/About';
+import Home from './components/pages/Home';
 
-function App() {
-  return <div className="App">My app</div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/about" component={About}></Route>
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
+  );
+};
 
 export default App;
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this app</h1>
+      <p className="my-1">This is full stack react app for keeping contacts</p>
+      <p className="bg-dark p">
+        <strong>Version: </strong>1.0.0
+      </p>
+    </Fragment>
+  );
+};
+
+export default About;
