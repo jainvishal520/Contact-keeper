dae60f2ec7d7958d3c65e007a72f50b80b748f1f
Vishal Jain <jainvishal520@gmail.com>
2019-12-14 16:39:41 +0530

    Contact Schema

---
+++ models/Contact.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const ContactSchema = mongoose.Schema({
+  // Each user have their set of won contacts
+  // Relationship between contact and user
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phone: {
+    type: String
+  },
+  type: {
+    type: String,
+    default: 'personal'
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('contact', ContactSchema);
