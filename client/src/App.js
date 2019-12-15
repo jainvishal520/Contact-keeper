3ba3b2505e8309ae5e75a2b3f75f17bf9e89f017
Vishal Jain <jainvishal520@gmail.com>
2019-12-15 21:09:40 +0530

    Contact and contact item component

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,23 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import About from './components/pages/About';
 import Home from './components/pages/Home';
+import ContactState from './context/contact/ContactState';
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home}></Route>
-            <Route path="/about" component={About}></Route>
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <ContactState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home}></Route>
+              <Route path="/about" component={About}></Route>
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   );
 };
 
